35b84dc46b31c7235676fbb4f06e0d32b9ddb460
Fauve <fauve.mareels@outlook.com>
2025-04-09 10:46:33 +0200

    Add to Favs and Computed Properties

--- app.js
+++ app.js
@@ -8,15 +8,26 @@ const app = Vue.createApp({
                 { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
                 { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
                 { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true}
-            ]
+            ],
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
         },
-            
+        toggleFav(book){
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
+        }
     }
 })
 
 app.mount('#app')
+
+//Challenge - Add to Favs
+// - attach a click event to each li tag (for each book)
+// - when a user clicks an li, toggle the 'isFav' property of that book
